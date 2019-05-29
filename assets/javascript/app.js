7aa49da161f3eee928273673cad92590ea252d20
AJW <ajweber102@gmail.com>
2019-05-29 16:09:02 -0400

    Logic setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,49 @@
-var time=20;
+var count=21;
 
-var counter=setInterval(timer, 1000); 
+var counter=setInterval(timer, 1000);
 
-function timeLeft()
-{
-  time=time-1;
-  if (time <= 0) {
+var score = 1;
+
+var counter = 1;
+
+var incorrect = 1;
+
+var correct = 0;
+
+// Timer function
+
+function timer() {
+  count = count - 1;
+  if (count <= -1)
+  {
      clearInterval(counter);
-     document.getElementById('time-left').innerHTML = time + " Seconds Left";
+     return;
   }
-}
+
+    document.getElementById("timer").innerHTML = count + " seconds left";
+};
+
+setTimeout(function() {
+    $('#ex1').modal('show');
+}, 21000); // milliseconds
+
+
+// Click function for Correct Answers
+
+$(".correct").on("click", function() {
+    $(this).css({color:'green'},{weight:'bold'});
+    $(".score").text((score ++ ) + " is your score"); {
+        counter = 0;
+        console.log(score + counter + "test")
+    }
+});
+
+// Click function for Incorrect Answers
+
+$(".incorrect").on("click", function() {
+    $(this).css({color:'red'},{weight:'bold'});
+    $(".wrong").text((incorrect ++ ) + " wrong answers"); {
+        counter = 0;
+        console.log(incorrect + counter + "test")
+    }
+});
